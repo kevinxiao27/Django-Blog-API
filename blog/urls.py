862ec59770a36b1c80257f17b1ae3055bfcb3707
kevinxiao27 <kevin.xiao27@gmail.com>
2023-01-05 17:11:07 -0700

    modified:   blog/urls.py 	modified:   templates/article.html 	modified:   templates/base.html 	modified:   templates/index.html

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('blog', views.PostList.as_view(), name='blog'),
     path('blog/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('projects/', views.ArticleList.as_view(), name='projects'),
+    path('projects/', views.ArticleList.as_view(), name='article'),
     path('projects/<slug:slug>/', views.ArticleContent.as_view(), name='article_detail'),
 ]
